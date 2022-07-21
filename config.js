6c439051b24b3f58fd30fcd50fd7bb9c49a93bc6
Bomiro <105790086+Bomiro@users.noreply.github.com>
2022-07-21 03:19:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -69,7 +69,7 @@ global.ntilink = []
 global.lolhuman = "KaysaS"
 global.sessionName = "session"
 global.antitags = true
-global.prefa = [','🐦','🐤','🗿']
+global.prefa = ['!','🐦','🐤','🗿','🐦']
 global.mess = {
     success: 'Done✓',
     admin: 'Tʜɪs Fᴇᴀᴛᴜʀᴇ Is Oɴʟʏ Fᴏʀ Aᴅᴍɪɴ',
